aba80add29e2fbef65abc423dc1d30ed90063a7a
Carmen Chico <CarmenChico@icloud.com>
2016-10-27 14:00:52 -0400

    added map to page

--- src/js/map.js
+++ src/js/map.js
@@ -1,4 +1,18 @@
+import { mapkey } from "./api-key";
 
 
-<iframe width="600" height="450" frameborder="0" style="border:0"
-src="https://www.google.com/maps/embed/v1/view?zoom=17&center=33.7723,-84.3662&key=..." allowfullscreen></iframe>
+function requestMap () {
+  return $.ajax({
+    url: `https://json-data.herokuapp.com/restaurant/menu/3`
+  });
+};
+
+
+var mapHTML=
+`<iframe 
+src="https://www.google.com/maps/embed/v1/view?zoom=17&center=33.7723,-84.3662&key=${mapkey}" allowfullscreen></iframe>`
+
+
+
+// width="150" height="100" frameborder="0" style="border:0"
+export { mapHTML };
