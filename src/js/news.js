2766898aad92154e692fb259a8636711d6cc3476
Carmen Chico <CarmenChico@icloud.com>
2016-10-28 13:13:06 -0400

    news

--- src/js/news.js
+++ src/js/news.js
@@ -8,21 +8,39 @@ function requestNews () {
 
 function extractNews (news) {
   var title = news.title;
-  var date = news.date_publised;
+  var date = news.date_published;
   var id= news.post_id;
   var blog= news.post;
 
 
-  return {
-    title: title,
-    date: date,
-    id: id,
-    blog: blog
-  }
+  // return {
+  //   title: title,
+  //   date: date,
+  //   id: id,
+  //   blog: blog
+  // }
+  var newsHTML = `
+    <h1>Latest News</h1>
+    <div class="newsTitle">
+      ${title}
+      <span>${date}</span>
+    </div>
+    <article class="blog">${blog}</article>
+    `;
+    console.log("HTML String", newsHTML);
+    $(".box4").append(newsHTML);
+
 };
 //this is the final function we use
 function getNewsData() {
   return requestNews().then(extractNews);
 };
 
+
 export {getNewsData};
+
+
+//////////////////
+function newsTemplate () {
+
+}
