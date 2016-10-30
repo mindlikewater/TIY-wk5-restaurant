64b938031ed27d54079acece49e186201b184ed0
Carmen Chico <CarmenChico@icloud.com>
2016-10-30 02:53:28 -0400

    added more css

--- src/js/map.js
+++ src/js/map.js
@@ -9,8 +9,9 @@ function requestMap () {
 
 
 var mapHTML=
-`<iframe 
-src="https://www.google.com/maps/embed/v1/view?zoom=17&center=33.7723,-84.3662&key=${mapkey}" allowfullscreen></iframe>`
+`<iframe
+src="https://www.google.com/maps/embed/v1/view?zoom=17&center=33.7723,-84.3662&key=${mapkey}" allowfullscreen></iframe>
+<p>777 Peachtree Street</p> <p>Atlanta, GA 30301   <span> 404-999-5555<span></p>`
 
 
 
