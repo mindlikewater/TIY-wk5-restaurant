e58461aa996b4575ea8a500129616225305918fc
Carmen Chico <CarmenChico@icloud.com>
2016-10-30 23:51:21 -0400

    fixed map location

--- src/js/map.js
+++ src/js/map.js
@@ -10,10 +10,10 @@ function requestMap () {
 
 var mapHTML=
 `<iframe
-src="https://www.google.com/maps/embed/v1/view?zoom=17&center=33.7723,-84.3662&key=${mapkey}" allowfullscreen></iframe>
+src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJ86lMh3gE9YgRWFFqMrlgCXE&key=${mapkey}" allowfullscreen></iframe>
 <p>777 Peachtree Street</p> <p>Atlanta, GA 30301   <span> 404-999-5555<span></p>`
 
-
+//<iframe frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJ86lMh3gE9YgRWFFqMrlgCXE&key=${mapkey}" allowfullscreen></iframe>
 
 // width="150" height="100" frameborder="0" style="border:0"
 export { mapHTML };
